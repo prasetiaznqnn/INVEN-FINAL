054a1999403d183b1d202336f8170a7ff9b9217d
prasetiaznqnn <if22.muhammadprasetia@mhs.ubpkarawang.ac.id>
2024-11-21 21:20:52 +0700

    update footer

--- footer.php
+++ footer.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <footer style="text-align: center;">
-        PT ELASTOMIX INDONESIA
-    </footer>
-</body>
-</html>
+    </div>
+    </div>
+    <script src="js/all.min2.js" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+    <script src="js/bootstrap.bundle2.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
